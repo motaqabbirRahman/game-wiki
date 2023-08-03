4bee81ed757e4324b2b8edd73a5921933103bf21
motaqabbirRahman <sunnojogfol@gmail.com>
2023-08-03 15:05:00 +0600

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Grid templateAreas={`"header header" "aside main"`}>
+      <GridItem area="header" bg="coral">
+        header
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        aside
+      </GridItem>
+      <GridItem area="main" bg="blue ">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
