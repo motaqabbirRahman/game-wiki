882a9a7c12479315db39ce14ea35d1a5daa62da9
motaqabbirRahman <sunnojogfol@gmail.com>
2023-08-20 01:51:05 +0600

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,10 +18,20 @@ export interface Platform {
   slug: string;
 }
 
-const useGames = (selectedGenre: Genre | null) => {
-  return useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) => {
+  return useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 };
 
 export default useGames;
