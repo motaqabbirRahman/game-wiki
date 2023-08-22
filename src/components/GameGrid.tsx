f5de1ae040ce084ce1ecbcae1d900428210ee3c8
motaqabbirRahman <sunnojogfol@gmail.com>
2023-08-23 00:25:08 +0600

    Refactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,26 @@
-import { Text } from "@chakra-ui/react";
-import useGames, { Game, Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
+import { GameQuery } from "../App";
+import { Text } from "@chakra-ui/react";
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCardSkeleton from "./GameCardSkeleton";
+import useGames, { Game } from "../hooks/useGames";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
-import { GameQuery } from "../App";
 
 interface Props {
   gameQuery: GameQuery;
 }
 const GameGrid = ({ gameQuery }: Props) => {
-  //where do I destucture it ?
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  if (error) return <Text>{error}</Text>;
+
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={7}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
